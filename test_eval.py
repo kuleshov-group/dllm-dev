0062b8993e362fe1e184f9f61a4bef8ef5e820e0
mariannearr <marianne.arriola@gmail.com>
2025-05-15 18:13:47 +0000

    zeroshot

--- test_eval.py
+++ test_eval.py
@@ -6,27 +6,27 @@ from composer.utils import dist, reproducibility
 from omegaconf import OmegaConf
 from streaming import StreamingDataset
 from transformers import AutoTokenizer
+import yaml
 
 from scripts.utils import (
     load_model_from_ckpt_dir_path,
     maybe_add_missing_special_tokens,
-    print_and_save_config,
     register_useful_resolvers,
 )
 
 register_useful_resolvers()
 
-DIR_NAME = "/home/ubuntu/qwen3_600M_gsm8k_ckpts"
 
+MODEL_DIR = "/home/ubuntu/runs/owt-block4-bs128-keepbottom21-keepevery1-causalencfalse-max1000000ba-lr1e-5-warmup1000ba-gc1.0-wd1e-5-bd3lm_qwen600M_v3"
 
-def main():
-    config = OmegaConf.load(os.path.join(DIR_NAME, "config.yaml"))
+def main(eval_dataset_name):
+    config = OmegaConf.load(os.path.join(MODEL_DIR, "config.yaml"))
+    # config.eval_dataloader.num_workers=128
+    
     reproducibility.seed_all(config.seed)
 
     config.composer.trainer.autoresume = False
     config.composer.trainer.save_folder = "/home/ubuntu/trash"
-
-    print_and_save_config(config, resolve=True, save_cfg=False)
 
     tokenizer = AutoTokenizer.from_pretrained(
         config.tokenizer.pretrained_model_name_or_path,
@@ -35,8 +35,8 @@ def main():
     tokenizer = maybe_add_missing_special_tokens(tokenizer)
 
     model = load_model_from_ckpt_dir_path(
-        path_to_ckpt_dir=DIR_NAME,
-        load_ema_weights=True,
+        path_to_ckpt_dir=MODEL_DIR,
+        load_ema_weights=False,
         ckpt_file="best-rank0.pt",
         verbose=True,
     ).to("cuda")
@@ -47,10 +47,19 @@ def main():
         metrics=list(hydra.utils.instantiate(config.metrics).values()),
     )
 
-    eval_dataset = hydra.utils.instantiate(
-        config.eval_dataset,
-        tokenizer=tokenizer,
-    )
+    if eval_dataset_name != "owt_eval":
+        with open(f"configs/dataset/{eval_dataset_name}.yaml", "r") as file:
+            eval_dataset = yaml.safe_load(file)
+
+        eval_dataset = hydra.utils.instantiate(
+            eval_dataset,
+            tokenizer=tokenizer,
+        )
+    else:
+        eval_dataset = hydra.utils.instantiate(
+            config.eval_dataset,
+            tokenizer=tokenizer,
+        )
 
     collator = hydra.utils.instantiate(config.collator, tokenizer=tokenizer)
     eval_sampler = (
@@ -76,7 +85,9 @@ def main():
     )
     metrics = trainer.eval()
     print(metrics)
+    print('EVAL DATASET', eval_dataset_name)
 
 
 if __name__ == "__main__":
-    main()
+    for eval_dataset_name in ["owt_eval", "ptb", "wikitext2", "lm1b-qwen", "lambada", "ag_news", "scientific-papers-pubmed", "scientific-papers-arxiv"]:
+        main(eval_dataset_name)
